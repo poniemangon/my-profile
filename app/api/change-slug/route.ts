1b8bc5f189a9b8adf566812b088f2beaecafcdef
poniemangon <108033301+poniemangon@users.noreply.github.com>
2025-10-17 11:54:36 -0300

    Update route.ts

--- app/api/change-slug/route.ts
+++ app/api/change-slug/route.ts
@@ -50,24 +50,7 @@ export async function POST(req: NextRequest) {
         if (error) {
             return NextResponse.json({ error: "Error al actualizar" }, { status: 500 });
         }
-        const oldQrCode = data[0]?.qr_code;
-        // Elimina toda la url del qr code excepto el nombre del archivo
-        const parts = oldQrCode?.split('/');
-        const fileName = parts?.[parts.length - 1];
-        const { data: removeError } = await supabase.storage
-        .from("qr-codes") // nombre del bucket
-        .remove([fileName]); // ruta(s) exacta(s) dentro del bucket
-        if (removeError) {
-            NextResponse.json({ error: "Error al eliminar el QR code" }, { status: 500 });
-        }
-        const qrCode = await generateAndSaveQRCode(newSlug);
-        const { error: updateError } = await supabase
-        .from("user_profiles")
-        .update({ qr_code: qrCode })
-        .eq("clerk_id", userId);
-        if (updateError) {
-            return NextResponse.json({ error: "Error al actualizar el QR code" }, { status: 500 });
-        }
+ 
 
         return NextResponse.json({ message: "Slug actualizado" });
     } catch (err) {
